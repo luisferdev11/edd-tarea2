542673cf41936753feab5d6856dec036b72d47e1
angeloffline <angeljose.ciencias.unam.mx>
2023-05-04 14:50:22 -0600

    Main con parametros

--- Main.java
+++ Main.java
@@ -1,8 +1,22 @@
+
 public class Main{
     public static void main(String[] args) {
-        Laberinto laberinto = new Laberinto(10, 10);
+        if(args.length<2){
+            System.err.println("Necesitas ingresar dos valores");
+            System.exit(1);
+        }
+        
+       
+        int x =Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+       
+        
+
+        Laberinto laberinto = new Laberinto(x, y);
         System.out.println(laberinto.toString());
         laberinto.generarLaberinto();
+        System.out.println(laberinto.toString());
+        laberinto.resolverLaberinto();
         System.out.println(laberinto.toString());
     }
 }
